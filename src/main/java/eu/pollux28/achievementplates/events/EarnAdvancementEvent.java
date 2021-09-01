bdf7ce1a1d71bc4ce6c255ee23d2707fb1285091
28Pollux28 <lemaire_valentin28@yahoo.fr>
2021-09-01 09:32:19 +0200

    fix #2

--- src/main/java/eu/pollux28/achievementplates/events/EarnAdvancementEvent.java
+++ src/main/java/eu/pollux28/achievementplates/events/EarnAdvancementEvent.java
@@ -20,7 +20,7 @@ public class EarnAdvancementEvent {
     public static AdvancementEarnCallBack onAdvancementEvent(){
         return ((advancement, playerEntity) -> {
             AdvancementDisplay display = advancement.getDisplay();
-            if(display !=null){
+            if(display !=null && display.shouldShowToast()){
                 NbtCompound nbt1 = new NbtCompound();
                 NbtCompound nbt2 = new NbtCompound();
                 writeDisplayToNbt(nbt2,display);
@@ -30,7 +30,7 @@ public class EarnAdvancementEvent {
                 itemStack.setTag(nbt1);
                 itemStack.setCustomName(playerEntity.getName().shallowCopy().setStyle(Style.EMPTY.withColor(Formatting.AQUA)).append(new LiteralText("'s ").setStyle(Style.EMPTY.withColor(Formatting.GRAY))).append(display.getTitle().shallowCopy().setStyle(Style.EMPTY.withColor(display.getFrame().getTitleFormat()))).append(new LiteralText(" trophy plate").setStyle(Style.EMPTY.withColor(Formatting.GRAY))));
                 if (playerEntity.giveItemStack(itemStack)) {
-                    playerEntity.world.playSound((PlayerEntity)null, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.PLAYERS, 0.2F, ((playerEntity.getRandom().nextFloat() - playerEntity.getRandom().nextFloat()) * 0.7F + 1.0F) * 2.0F);
+                    playerEntity.world.playSound(null, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.PLAYERS, 0.2F, ((playerEntity.getRandom().nextFloat() - playerEntity.getRandom().nextFloat()) * 0.7F + 1.0F) * 2.0F);
                 } else {
                     ItemEntity itemEntity = playerEntity.dropItem(itemStack, false);
                     if (itemEntity != null) {
